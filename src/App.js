c859f998f02f7d68d543b8452349fcf6eb783f7f
FinoDevLaptop <kieuvantrung.dev@gmail.com>
2024-04-27 11:18:05 +0700

    MERN E-Commerce(Coder Dost)---01:06:42

--- src/App.js
+++ src/App.js
@@ -1,57 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import ProductList from "./features/product-list/ProductList";
+import "./index.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Card from "./features/card/Card";
+import Checkout from "./pages/CheckoutPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/card" element={<Card />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<div>Error</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/CardPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../features/card/Card";
+
+const CardPage = () => {
+  return (
+    <div>
+      <Card></Card>
+    </div>
+  );
+};
+
+export default CardPage;
